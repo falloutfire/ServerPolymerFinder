ef5e332476324d5a27d1d277ab40267af8e000cb
falloutfire <lihach77@gmail.com>
2020-05-05 18:31:54 +0300

    cors

--- src/main/kotlin/com/kleknerpolymer/server/ServerApplication.kt
+++ src/main/kotlin/com/kleknerpolymer/server/ServerApplication.kt
@@ -2,10 +2,31 @@ package com.kleknerpolymer.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletResponse
+
 
 @SpringBootApplication
 class ServerApplication
 
 fun main(args: Array<String>) {
 	runApplication<ServerApplication>(*args)
+}
+
+@Component
+class SimpleCORSFilter : Filter {
+	@Throws(IOException::class, ServletException::class)
+	override fun doFilter(req: ServletRequest?, res: ServletResponse, chain: FilterChain) {
+		val response = res as HttpServletResponse
+		response.setHeader("Access-Control-Allow-Origin", "*")
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+		response.setHeader("Access-Control-Max-Age", "3600")
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with")
+		chain.doFilter(req, res)
+	}
+
+	override fun init(filterConfig: FilterConfig?) {}
+	override fun destroy() {}
 }
