7102f6139b910d292e3045c68371b747fcdd2b5a
nonameilya <liv@nonamelab.ru>
2020-06-13 17:14:01 +0300

    cors headesrs, add put

--- src/main/kotlin/com/kleknerpolymer/server/ServerApplication.kt
+++ src/main/kotlin/com/kleknerpolymer/server/ServerApplication.kt
@@ -21,7 +21,7 @@ class SimpleCORSFilter : Filter {
 	override fun doFilter(req: ServletRequest?, res: ServletResponse, chain: FilterChain) {
 		val response = res as HttpServletResponse
 		response.setHeader("Access-Control-Allow-Origin", "*")
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
 		response.setHeader("Access-Control-Max-Age", "3600")
 		response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 		chain.doFilter(req, res)
